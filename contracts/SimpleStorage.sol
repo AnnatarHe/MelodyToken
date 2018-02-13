2ff2ed230797cea9822e9d3851a8a80321b72037
le.he <Annatar.He@liulishuo.com>
2018-02-13 18:05:02 +0800

    add erc223 interface

--- contracts/SimpleStorage.sol
+++ contracts/SimpleStorage.sol
@@ -1,13 +1,10 @@
 pragma solidity ^0.4.18;
+import "./ERC223-interface.sol";
 
-contract SimpleStorage {
-  uint storedData;
-
-  function set(uint x) public {
-    storedData = x;
-  }
-
-  function get() public view returns (uint) {
-    return storedData;
-  }
+contract SimpleStorage is ERC223 {
+  uint public totalSupply;
+  function balanceOf(address who) constant returns (uint);
+  function transfer(address to, uint value);
+  function transfer(address to, uint value, bytes data);
+  event Transfer(address indexed from, address indexed to, uint value, bytes data);
 }
