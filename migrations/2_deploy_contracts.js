9aa084c04102e603ea18bf74643b369ebd7fe559
le.he <Annatar.He@liulishuo.com>
2018-02-22 21:36:07 +0800

    remove build directory and deploy contract to local

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,13 +1,10 @@
 var MelodyToken = artifacts.require("./MelodyToken.sol");
-var MelodyTokeenCrowdsale = artifacts.require('./Crowdsale.sol');
+var MelodyTokenCrowdsale = artifacts.require('./Crowdsale.sol');
 
 module.exports = function(deployer, network, accounts) {
-
-  console.log('--'.repeat(10))
-  console.log(network, accounts)
-  console.log('--'.repeat(10))
-
   // 现在测试，先转到第三个账户上
   const sendTo = accounts[2];
-  deployer.deploy(MelodyToken, 1000, sendTo, MelodyToken.address);
+  deployer.deploy(MelodyToken).then(() => {
+    return deployer.deploy(MelodyTokenCrowdsale, 1000, sendTo, MelodyToken.address);
+  })
 };
